3ac9bb2fc2792634bf169910b1ee8b905d4b6689
patty-rosendaal <patty.rosendaal@student.hu.nl>
2019-10-01 09:48:43 +0200

    added default e value 0.01 to tblastn function

--- scpackage/R/tblastn.R
+++ scpackage/R/tblastn.R
@@ -9,11 +9,11 @@
 #'
 #' @examples
 #' tblastn()
-tblastn = function(blast_db, input, out){
+tblastn = function(blast_db, input, out, e = 0.01){
   tblastn = "inst/programs/ncbi-blast-2.9.0+/bin/tblastn"
   format = 6 #tabular output
   #run tblastn
-  command = paste0(tblastn, " -db ", blast_db, " -query ", input, " -out ", out)
+  command = paste0(tblastn, " -db ", blast_db, " -query ", input, " -out ", out, " -evalue ", e)
   tblastn_out = system(command, wait = TRUE)
   #tblastn_out = read.table(textConnection(tblastn_out))
   return(tblastn_out)
@@ -21,6 +21,6 @@ tblastn = function(blast_db, input, out){
 
 #inst/extdata/M.6\ M.17\ IR1.txt escaping spaces in original filename
 
-tblastn("data/IR1_nucl_db/IR1_nucl_db", "inst/extdata/'M.6 M.17 IR1.txt'", "M17_IR1_tblastn")
+tblastn("data/IR1_nucl_db/IR1_nucl_db", "inst/extdata/'M.6 M.17 IR1.txt'", "M17_IR1_tblastn", 0.01)
 system("mv M17_IR1_tblastn data")
 
