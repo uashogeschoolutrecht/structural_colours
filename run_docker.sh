b539fda38f0f6791471c89ffdab59c57008f0d8b
patty-rosendaal <54795739+patty-rosendaal@users.noreply.github.com>
2019-11-21 14:03:25 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -19,7 +19,7 @@ PASSWD=SoSecret
 PACK_DIR=${PWD}/scpackage
 DRIVE_DIR=/home/patty_rosendaal/local_storage
 
-sudo docker run -d --rm -p 28786:8787 --name geo_testing2 -e USERID=rstudio -e PASSWORD=$PASSWD \
+sudo docker run -d --rm -p 28786:8787 --name geo_testing2 -e USERID=$UID -e PASSWORD=$PASSWD \
 -v $PACK_DIR:/home/rstudio/scpackage -v $DRIVE_DIR:/home/rstudio/research_drive -v /data:/home/rstudio/data rstudio/pack-testing2
 
 #
